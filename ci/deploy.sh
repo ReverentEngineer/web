dd7ea8e5a6ea5ccb98582853b5a512e5f196a325
Jeff Caffrey-Hill <jeff@reverentengineer.com>
2020-07-30 11:25:52 -0400

    Fix CI script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-DEPLOY_DIR=${GITHUB_WORKSPACE}/public
+DEPLOY_DIR=${GITHUB_WORKSPACE}/../public
 SOURCE_DIR=${GITHUB_WORKSPACE}
 cd $DEPLOY_DIR && git rm . && cd $SOURCE_DIR
 curl -L  https://github.com/gohugoio/hugo/releases/download/v0.74.3/hugo_0.74.3_Linux-64bit.tar.gz | tar -x hugo
 hugo -d $DEPLOY_DIR
 cd $DEPLOY_DIR
 git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git config --local user.action "${GITHUB_ACTOR}"
+git config --local user.name "${GITHUB_ACTOR}"
 git add .
 git commit -m "Deployment for $(date +%Y%m%d-%H:%M:%s)"
-git push origin master
+git push -u origin master
