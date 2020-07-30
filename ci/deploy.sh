8a017926dbec6a629831aa17adae17b5125898d2
Jeff Caffrey-Hill <jeff@reverentengineer.com>
2020-07-30 11:45:47 -0400

    Clone repository manually in CI.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git clone https://${GITHUB_TOKEN}@github.com/ReverentEngineer/ReverentEngineer.github.io public || exit 1
 cd public && git rm -r . && cd .. || exit 1
 curl -L  https://github.com/gohugoio/hugo/releases/download/v0.74.3/hugo_0.74.3_Linux-64bit.tar.gz | tar -xz hugo || exit 1
 ./hugo || exit 1
