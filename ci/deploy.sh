eca5c4d2a253d6c6616c254e9427737088bc6115
Jeff Caffrey-Hill <jeff@reverentengineer.com>
2020-07-30 11:29:29 -0400

    More small CI tweaks

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DEPLOY_DIR=${GITHUB_WORKSPACE}/../public
 SOURCE_DIR=${GITHUB_WORKSPACE}
-cd $DEPLOY_DIR && git rm . && cd $SOURCE_DIR
+cd $DEPLOY_DIR && git rm -r . && cd $SOURCE_DIR
 curl -L  https://github.com/gohugoio/hugo/releases/download/v0.74.3/hugo_0.74.3_Linux-64bit.tar.gz | tar -xz hugo || exit 1
-hugo -d $DEPLOY_DIR || exit 1
+./hugo -d $DEPLOY_DIR || exit 1
 cd $DEPLOY_DIR || exit 1
 git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git config --local user.name "${GITHUB_ACTOR}"
