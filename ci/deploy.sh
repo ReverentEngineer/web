7fd38e493b1742f8bc8009c41fbe8823216d1521
Jeff Caffrey-Hill <jeff@reverentengineer.com>
2020-07-30 11:26:31 -0400

    De-compress archive.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,7 +2,7 @@
 DEPLOY_DIR=${GITHUB_WORKSPACE}/../public
 SOURCE_DIR=${GITHUB_WORKSPACE}
 cd $DEPLOY_DIR && git rm . && cd $SOURCE_DIR
-curl -L  https://github.com/gohugoio/hugo/releases/download/v0.74.3/hugo_0.74.3_Linux-64bit.tar.gz | tar -x hugo
+curl -L  https://github.com/gohugoio/hugo/releases/download/v0.74.3/hugo_0.74.3_Linux-64bit.tar.gz | tar -xz hugo
 hugo -d $DEPLOY_DIR
 cd $DEPLOY_DIR
 git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
