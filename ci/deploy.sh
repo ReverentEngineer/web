f23e3af71f962f134d214dd2e2f6f5b3aeef2192
Jeff Caffrey-Hill <jeff@reverentengineer.com>
2020-07-30 11:00:09 -0400

    Add username and no-reply e-mail to commit,

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git -C public config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git -C public config --local user.action "${GITHUB_ACTOR}"
 git -C public rm .
 hugo
 git -C public add .
